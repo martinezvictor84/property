7fecfb82a42adb00afd6ff848959de9cb4072ef5
Victor Martinez <victor.martinez@yuju.io>
2022-04-11 16:37:54 -0500

    build property_service

--- services/property_service.py
+++ services/property_service.py
@@ -1,6 +1,29 @@
+from .habi_db import HabiDb
+
+
+def get_filter(params):
+    filter = {}
+    if params.get('status_id'):
+        filter['status_id'] = params.get('status_id')
+    if params.get('year'):
+        filter['year'] = params.get('year')
+    if params.get('city'):
+        filter['city'] = params.get('city')
+    return filter
+
+
 class PropertyService:
     def __init__(self, settings):
         self.setting = settings
+        self.habi_db = HabiDb(**settings)
 
-    def get(self):
-        pass
+    def get_properties(self, params):
+        try:
+            filters = get_filter(params)
+        except Exception as e:
+            raise FilterError(str(e))
+        return self.habi_db.get_properties(filters)
+
+
+class FilterError(Exception):
+    pass
